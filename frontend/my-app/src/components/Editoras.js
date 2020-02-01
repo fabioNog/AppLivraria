4a939d8a469488c8c9996d629e38b2247d0e553e
FabioNog <fabio.carmo@computacao.ufla.br>
2020-02-01 04:09:55 -0300

    Alter component Create filme

--- frontend/my-app/src/components/Editoras.js
+++ frontend/my-app/src/components/Editoras.js
@@ -1,9 +1,66 @@
-import React, {Component} from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
 
-class Editoras extends Component{
-    render(){
-        return(<h1>Editoras</h1>)
-    }
+const currencies = [
+    {
+        value: 'USD',
+        label: '$',
+    },
+    {
+        value: 'EUR',
+        label: '€',
+    },
+    {
+        value: 'BTC',
+        label: '฿',
+    },
+
+];
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: 200,
+        },
+    },
+}));
+
+export default function MultilineTextFields() {
+    const classes = useStyles();
+    const [currency, setCurrency] = React.useState('EUR');
+    
+
+    const handleChange = event => {
+        setCurrency(event.target.value);
+    };
+
+    return (
+        <form className={classes.root} noValidate autoComplete="off">
+            <div>
+                <TextField
+                    id="standard-select-currency"
+                    select
+                    label="Select"
+                    value={currency}
+                    onChange={handleChange}
+                    helperText="Please select your currency"
+                >
+                    {currencies.map(option => (
+                        <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+                </TextField>               
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </form>
+    );
 }
-
-export default Editoras
--- frontend/my-app/src/components/Livros.js
+++ frontend/my-app/src/components/Livros.js
@@ -1,9 +1,185 @@
-import React, {Component} from 'react'
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import MenuItem from '@material-ui/core/MenuItem';
 
-class Filme extends Component{
-    render(){
-        return(<h1>Livros</h1>)
-    }
+//Data Picker
+import {
+    MuiPickersUtilsProvider,
+    KeyboardTimePicker,
+    KeyboardDatePicker,
+  } from '@material-ui/pickers';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://soitic.com.br/">
+                Livraria Grupo Soitic
+      </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
 
-export default Filme
+const useStyles = makeStyles(theme => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+const currencies = [
+    {
+        value: 'USD',
+        label: 'Comedia',
+    },
+    {
+        value: 'EUR',
+        label: 'Suspense',
+    },
+    {
+        value: 'BTC',
+        label: 'Ficcão',
+    },
+    {
+        value: 'BTC',
+        label: 'Terror',
+    },    
+    {
+        value: 'BTC',
+        label: 'Romance',
+    },
+    {
+        value: 'BTC',
+        label: 'acao',
+    },
+
+];
+
+export default function SignIn() {
+    const classes = useStyles();
+    const [currency, setCurrency] = React.useState('EUR');
+
+    const handleChange = event => {
+        setCurrency(event.target.value);
+    };
+
+    //Data Piker
+    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Cadastre seu livro:
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Nome do Filme"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        id="standard-select-currency"
+                        select
+                        label="Categoria"
+                        value={currency}
+                        onChange={handleChange}
+                        helperText="Por favor selecione a categoria"
+                        autoFocus
+                        className={classes.form}
+                    >
+                        {currencies.map(option => (
+                            <MenuItem key={option.value} value={option.value}>
+                                {option.label}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        width="100%"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Cadastrar
+          </Button>
+                </form>
+            </div>
+            <Box mt={8}>
+                <Copyright />
+            </Box>
+        </Container>
+    );
+}
