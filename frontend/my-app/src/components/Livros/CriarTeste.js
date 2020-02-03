9726d39bb7ecb913281d56a5e5ad76bdfd27b9fb
FabioNog <fabio.carmo@computacao.ufla.br>
2020-02-03 19:15:45 -0300

    Alter my criar livro frontend

--- frontend/my-app/src/components/Livros/CriarTeste.js
+++ frontend/my-app/src/components/Livros/CriarTeste.js
@@ -1,64 +1,69 @@
-import React, { useState,useEffect } from 'react';
+import React, { Component,useState,useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import MenuItem from '@material-ui/core/MenuItem';
 import axios from 'axios'
 
-function Teste(){
-const [vet, setVet] = useState([])
-  useEffect(() =>{
-    axios({
-      method: 'GET',
-      url: 'https://jsonplaceholder.typicode.com/posts'
-    }).then(res => {
-      setVet(res.data)
-    })
-  })  
-  return(
-    <div>
-      {vet.map((vet,i) => (
-        <div id={vet.id}>{vet.title}</div>
-      ))}
-    </div>
-  )
-}
-export default Teste
-
-
-/*const useStyles = makeStyles(theme => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+const currencies = [
+  {
+    value: 'CM',
+    label: 'Comedia',
   },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
+  {
+    value: 'SP',
+    label: 'Suspense',
   },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
+  {
+    value: 'FC',
+    label: 'Ficção',
   },
-}));
+  {
+    value: 'TR',
+    label: 'Terror',
+  },
+  {
+    value: 'RM',
+    label: 'Romance',
+  },
+  {
+    value: 'PC',
+    label: 'Policial',
+  },
+  {
+    value: 'AC',
+    label: 'Ação',
+  },
+];
 
-export default function CriarTeste() {
-  const classes = useStyles();
-  const [emailTeste, setEmailTeste] = useState("");
-  const [passwordTeste, setPasswordTeste] = useState("");
-  
-  const handleChangeEmail = e => {
-    setEmailTeste(e.target.emailTeste)
-  }
-  const handleChangePassword = e => {
-    setEmailTeste(e.target.passwordTeste)
+class Criar extends Component{
+
+  constructor(props){
+    super(props)
+    this.state ={
+      nome: '',
+      data: '',
+      categoria: '',
+      sinopse: '',
+    }
+    this.handleChangeName = this.handleChangeName.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
 
-  useEffect(() => console.log(emailTeste), [emailTeste]);
+  handleChangeName(e){
+    this.setState({name: e.target.value})
+  }
 
-  return (
-    <Container component="main" maxWidth="xs">
+  handleClick(){
+    console.log(this.state.name)
+  }
+
+  render(){
+    const classes = this.props
+    return(
+      <Container component="main" maxWidth="xs" style={{padding: '60px'}}>
       <CssBaseline />
       <div className={classes.paper}>
         <form className={classes.form}>
@@ -67,35 +72,64 @@ export default function CriarTeste() {
             margin="normal"
             required
             fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
+            label="Nome do Livro"
+            autoComplete="Nome do Livro"
             autoFocus
-            onChange={handleChangeEmail}            
+            onChange={this.handleChangeName}
           />
           <TextField
             variant="outlined"
-            margin="normal"
-            required
+            id="date"
+            label="Data de Criação"
+            type="date"
+            autoFocus
+            InputLabelProps={{
+              shrink: true,
+            }}
             fullWidth
-            name="password"
-            label="Password"
-            id="password"
-            autoComplete="current-password"
-            onChange={handleChangePassword}
+            style={{marginTop: '10px'}}
+          />
+          <TextField
+            id="outlined-select-currency"
+            select
+            label="Categoria"
+            helperText="Selecione a categoria"
+            variant="outlined"
+            fullWidth
+            style={{marginTop: '10px'}}
+          >
+            {currencies.map(option => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            id="outlined-multiline-static"
+            label="Sinopse"
+            multiline
+            rows="4"
+            variant="outlined"
+            fullWidth
+            style={{marginTop: '15px'}}
           />
           <Button
             type="submit"
             fullWidth
             variant="contained"
             color="primary"
-            className={classes.submit}           
+            className={classes.submit}
+            onClick={this.handleClick}
+            style={{paddingTop: '15px',marginTop: '10px'}}
           >
             Sign In
           </Button>
         </form>
       </div>
-    </Container>
-  );
-}*/
+    </Container>      
+    )
+  }
+}
+
+export default Criar
+
